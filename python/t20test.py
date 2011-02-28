e1242d04894742968836f3cc8d3830744f0398e5
Thammi <thammi@chaossource.net>
2011-03-01 00:30:38 +0100

    adding initialization and possible approach to drawing

--- python/t20test.py
+++ python/t20test.py
@@ -1,6 +1,8 @@
 import usb.core
 import usb.util
 
+import initdata
+
 # find our device
 dev = usb.core.find(idVendor=0x08ca, idProduct=0x2137)
 
@@ -12,18 +14,17 @@ if dev is None:
 # configuration will be the active one
 dev.set_configuration()
 
-# get an endpoint instance
-ep = usb.util.find_descriptor(
-        dev.get_interface_altsetting(),   # first interface
-        # match the first OUT endpoint
-        custom_match = \
-            lambda e: \
-                usb.util.endpoint_direction(e.bEndpointAddress) == \
-                usb.util.ENDPOINT_OUT
-    )
 
-assert ep is not None
+# send the initial foo
+for line in initdata.data:
+    dev.write(3, ''.join(chr(i) for i in line[17:]))
 
-# write the data
-ep.write('test')
+# non-working drawing
+
+#start = [0x11, 0x00, 0x00, 0x00, 0x00, 0x80, 0x02, 0xe0, 0x01, 0x80, 0x02, 0xe0, 0x01, 0x00, 0x40, 0x00, 0x40, 0x00, 0x00, 0x10, 0x0e]
+
+#dev.write(3, ''.join(chr(i) for i in start))
+
+#for i in range(1800):
+    #dev.write(3, ''.join(chr(0) for i in range(512)))
 
