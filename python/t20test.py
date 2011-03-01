cb9257f21a6360bb707157aaad554447bd4e487f
Thammi <thammi@chaossource.net>
2011-03-01 05:41:09 +0100

    Code cleanup

--- python/t20test.py
+++ python/t20test.py
@@ -6,11 +6,6 @@ from initdata import *
 def pack(raw):
     return ''.join(chr(i) for i in raw)
 
-def run(lines):
-    for line in lines:
-        print ', '.join(hex(i) for i in line[8*3:])
-        dev.write(3, pack(line[8*3:]))
-
 # find our device
 dev = usb.core.find(idVendor=0x08ca, idProduct=0x2137)
 
@@ -22,24 +17,36 @@ if dev is None:
 # configuration will be the active one
 dev.set_configuration()
 
+
+interface = dev[0][(0,0)]
+
+inp = interface[0]
+raw = interface[1]
+cmd = interface[2]
+
+def run(lines):
+    for line in lines:
+        print ', '.join(hex(i) for i in line[8*3:])
+        cmd.write(pack(line[8*3:]))
+
 run(phase0)
 
 print "writing null bytes"
 
 nullcmd = [0x11, 0x00, 0x00, 0x00, 0x00, 0xa0, 0x00, 0x78, 0x00, 0x80, 0x02, 0xe0, 0x01, 0x00, 0x10, 0x00, 0x10, 0x04, 0x00, 0x96, 0x00]
 
-dev.write(2, pack(nullcmd))
+raw.write(pack(nullcmd))
 
 for i in range(77):
-    dev.write(2, ''.join(chr(0) for i in range(512)))
+    raw.write(''.join(chr(0) for i in range(512)))
 
 run(phase1)
 
-print "response", dev.read(1, 1)
+print inp.read(1)
 
 run(phase2)
 
-print "response", dev.read(1, 1)
+print inp.read(1)
 
 run(phase3)
 
@@ -47,8 +54,8 @@ run(phase3)
 
 start = [0x11, 0x00, 0x00, 0x00, 0x00, 0x80, 0x02, 0xe0, 0x01, 0x80, 0x02, 0xe0, 0x01, 0x00, 0x40, 0x00, 0x40, 0x00, 0x00, 0x10, 0x0e]
 
-dev.write(2, pack(start))
+raw.write(pack(start))
 
 for i in range(1800):
-    dev.write(2, ''.join(chr(0xFF) for i in range(512)))
+    raw.write(''.join(chr(0xFF) for i in range(512)))
 
