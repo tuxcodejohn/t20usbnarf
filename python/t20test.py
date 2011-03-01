dfafe4436ea118686e570c266500ffc83ac93f07
Thammi <thammi@chaossource.net>
2011-03-01 07:11:47 +0100

    Cleaning up init sequence

--- python/t20test.py
+++ python/t20test.py
@@ -29,7 +29,18 @@ def run(lines):
         print ', '.join(hex(i) for i in line[8*3:])
         cmd.write(pack(line[8*3:]))
 
-run(phase0)
+def batch(lines):
+    for line in lines:
+        print ', '.join(hex(i) for i in line)
+
+        cmd.write(pack(line))
+
+        if line[0] == 0x05:
+            print "read:", hex(inp.read(1)[0])
+        elif line[0] == 0x25:
+            print "read: ", inp.read(512)
+
+batch(phase0)
 
 print "writing null bytes"
 
@@ -37,18 +48,11 @@ nullcmd = [0x11, 0x00, 0x00, 0x00, 0x00, 0xa0, 0x00, 0x78, 0x00, 0x80, 0x02, 0xe
 
 raw.write(pack(nullcmd))
 
-for i in range(77):
+# TODO: check whether this is the right amount!
+for i in range(75):
     raw.write(''.join(chr(0) for i in range(512)))
 
-run(phase1)
-
-print inp.read(1)
-
-run(phase2)
-
-print inp.read(1)
-
-run(phase3)
+batch(phase1)
 
 # non-working drawing
 
@@ -56,6 +60,11 @@ start = [0x11, 0x00, 0x00, 0x00, 0x00, 0x80, 0x02, 0xe0, 0x01, 0x80, 0x02, 0xe0,
 
 raw.write(pack(start))
 
+import random
+r = random.Random()
+
 for i in range(1800):
-    raw.write(''.join(chr(0xFF) for i in range(512)))
+    raw.write(''.join(chr(0xff if i%3 else 0x00) for i in range(512)))
+
+batch(post)
 
