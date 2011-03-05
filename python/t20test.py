43b19684b4a180cede7dc4a35971a2a15066a648
Thammi <thammi@chaossource.net>
2011-03-05 02:15:34 +0100

    Preserving aspect ratio for image viewing

--- python/t20test.py
+++ python/t20test.py
@@ -83,10 +83,22 @@ def image_pixel(image):
         for color in reversed(pixel):
             yield color
 
+target_res = (640, 480)
+
 if len(sys.argv) > 1:
-    im = Image.open(sys.argv[1]).resize((640,480))
+    orig = Image.open(sys.argv[1])
+
+    scale = min(float(target) / old for old, target in zip(orig.size, target_res))
+    new_size = tuple(int(old * scale) for old in orig.size)
+
+    scaled = orig.resize(new_size)
+
+    top = tuple((target - new) / 2 for new, target in zip(new_size, target_res))
+
+    im = Image.new(mode="RGB", size=target_res)
+    im.paste(scaled, top)
 else:
-    im = Image.new(mode="RGB", size=(640,480))
+    im = Image.new(mode="RGB", size=target_res)
 
     draw = ImageDraw.Draw(im)
     draw.text((5, 5), "hello world!")
