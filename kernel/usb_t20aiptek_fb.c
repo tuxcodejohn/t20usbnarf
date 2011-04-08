64e176c0b80297181cb59a1c7c797b5c23f942e4
john <johnATtuxcodeDOTorg>
2011-04-08 17:42:35 +0200

    framebuffer setup shice done... hope it worx
    still much to do in usb stuffz

--- kernel/usb_t20aiptek_fb.c
+++ kernel/usb_t20aiptek_fb.c
@@ -113,8 +113,6 @@ struct usb_t20aiptek {
 	__u8			bulk_raw_endpointAddr;
 	__u8 			bulk_command_endpointAddr;
 	int			errors;			/* the last request tanked */
-//	int			open_count;		/* count the number of openers */
-//	bool			ongoing_read;		/* a read is going on */
 	bool			processed_urb;		/* indicates we haven't processed the urb */
 	spinlock_t		err_lock;		/* lock for errors */
 	struct kref		kref;
@@ -124,8 +122,6 @@ struct usb_t20aiptek {
 	struct fb_info		info;			/* frame buffer info */
 	void			*fb_virt;	/* virt. address of the frame buffer */
 	dma_addr_t		fb_phys;	/* phys. address of the frame buffer */
-	int			fb_alloced;	/* Flag, was the fb memory alloced? */
-
 	
 	u32		pseudo_palette[PALETTE_ENTRIES_NO];
 					/* Fake palette of 16 colors */
@@ -137,7 +133,7 @@ struct usb_t20aiptek {
 static struct usb_driver t20aiptek_driver;
 static void t20aiptek_draw_down(struct usb_t20aiptek *dev);
 
-static void t20aiptek_delete(struct kref *kref)
+static void t20_delete(struct kref *kref)
 {
 	struct usb_t20aiptek *dev = to_t20aiptek_dev(kref);
 
@@ -147,444 +143,9 @@ static void t20aiptek_delete(struct kref *kref)
 	kfree(dev);
 }
 
-static int t20aiptek_open(struct inode *inode, struct file *file)
-{
-	struct usb_t20aiptek *dev;
-	struct usb_interface *interface;
-	int subminor;
-	int retval = 0;
 
-	subminor = iminor(inode);
 
-	interface = usb_find_interface(&t20aiptek_driver, subminor);
-	if (!interface) {
-		err("%s - error, can't find device for minor %d",
-		     __func__, subminor);
-		retval = -ENODEV;
-		goto exit;
-	}
-
-	dev = usb_get_intfdata(interface);
-	if (!dev) {
-		retval = -ENODEV;
-		goto exit;
-	}
-
-	/* increment our usage count for the device */
-	kref_get(&dev->kref);
-
-	/* lock the device to allow correctly handling errors
-	 * in resumption */
-	mutex_lock(&dev->io_mutex);
-
-	if (!dev->open_count++) {
-		retval = usb_autopm_get_interface(interface);
-			if (retval) {
-				dev->open_count--;
-				mutex_unlock(&dev->io_mutex);
-				kref_put(&dev->kref, t20aiptek_delete);
-				goto exit;
-			}
-	} /* else { //uncomment this block if you want exclusive open
-		retval = -EBUSY;
-		dev->open_count--;
-		mutex_unlock(&dev->io_mutex);
-		kref_put(&dev->kref, t20aiptek_delete);
-		goto exit;
-	} */
-	/* prevent the device from being autosuspended */
-
-	/* save our object in the file's private structure */
-	file->private_data = dev;
-	mutex_unlock(&dev->io_mutex);
-
-exit:
-	return retval;
-}
-
-static int t20aiptek_release(struct inode *inode, struct file *file)
-{
-	struct usb_t20aiptek *dev;
-
-	dev = file->private_data;
-	if (dev == NULL)
-		return -ENODEV;
-
-	/* allow the device to be autosuspended */
-	mutex_lock(&dev->io_mutex);
-	if (!--dev->open_count && dev->interface)
-		usb_autopm_put_interface(dev->interface);
-	mutex_unlock(&dev->io_mutex);
-
-	/* decrement the count on our device */
-	kref_put(&dev->kref, t20aiptek_delete);
-	return 0;
-}
-
-static int t20aiptek_flush(struct file *file, fl_owner_t id)
-{
-	struct usb_t20aiptek *dev;
-	int res;
-
-	dev = file->private_data;
-	if (dev == NULL)
-		return -ENODEV;
-
-	/* wait for io to stop */
-	mutex_lock(&dev->io_mutex);
-	t20aiptek_draw_down(dev);
-
-	/* read out errors, leave subsequent opens a clean slate */
-	spin_lock_irq(&dev->err_lock);
-	res = dev->errors ? (dev->errors == -EPIPE ? -EPIPE : -EIO) : 0;
-	dev->errors = 0;
-	spin_unlock_irq(&dev->err_lock);
-
-	mutex_unlock(&dev->io_mutex);
-
-	return res;
-}
-
-static void t20aiptek_read_bulk_callback(struct urb *urb)
-{
-	struct usb_t20aiptek *dev;
-
-	dev = urb->context;
-
-	spin_lock(&dev->err_lock);
-	/* sync/async unlink faults aren't errors */
-	if (urb->status) {
-		if (!(urb->status == -ENOENT ||
-		    urb->status == -ECONNRESET ||
-		    urb->status == -ESHUTDOWN))
-			err("%s - nonzero write bulk status received: %d",
-			    __func__, urb->status);
-
-		dev->errors = urb->status;
-	} else {
-		dev->bulk_in_filled = urb->actual_length;
-	}
-	dev->ongoing_read = 0;
-	spin_unlock(&dev->err_lock);
-
-	complete(&dev->bulk_in_completion);
-}
-
-static int t20aiptek_do_read_io(struct usb_t20aiptek *dev, size_t count)
-{
-	int rv;
-
-	/* prepare a read */
-	usb_fill_bulk_urb(dev->bulk_in_urb,
-			dev->udev,
-			usb_rcvbulkpipe(dev->udev,
-				dev->bulk_in_endpointAddr),
-			dev->bulk_in_buffer,
-			min(dev->bulk_in_size, count),
-			t20aiptek_read_bulk_callback,
-			dev);
-	/* tell everybody to leave the URB alone */
-	spin_lock_irq(&dev->err_lock);
-	dev->ongoing_read = 1;
-	spin_unlock_irq(&dev->err_lock);
-
-	/* do it */
-	rv = usb_submit_urb(dev->bulk_in_urb, GFP_KERNEL);
-	if (rv < 0) {
-		err("%s - failed submitting read urb, error %d",
-			__func__, rv);
-		dev->bulk_in_filled = 0;
-		rv = (rv == -ENOMEM) ? rv : -EIO;
-		spin_lock_irq(&dev->err_lock);
-		dev->ongoing_read = 0;
-		spin_unlock_irq(&dev->err_lock);
-	}
-
-	return rv;
-}
-
-static ssize_t t20aiptek_read(struct file *file, char *buffer, size_t count,
-			 loff_t *ppos)
-{
-	struct usb_t20aiptek *dev;
-	int rv;
-	bool ongoing_io;
-
-	dev = file->private_data;
-
-	/* if we cannot read at all, return EOF */
-	if (!dev->bulk_in_urb || !count)
-		return 0;
-
-	/* no concurrent readers */
-	rv = mutex_lock_interruptible(&dev->io_mutex);
-	if (rv < 0)
-		return rv;
-
-	if (!dev->interface) {		/* disconnect() was called */
-		rv = -ENODEV;
-		goto exit;
-	}
-
-	/* if IO is under way, we must not touch things */
-retry:
-	spin_lock_irq(&dev->err_lock);
-	ongoing_io = dev->ongoing_read;
-	spin_unlock_irq(&dev->err_lock);
-
-	if (ongoing_io) {
-		/* nonblocking IO shall not wait */
-		if (file->f_flags & O_NONBLOCK) {
-			rv = -EAGAIN;
-			goto exit;
-		}
-		/*
-		 * IO may take forever
-		 * hence wait in an interruptible state
-		 */
-		rv = wait_for_completion_interruptible(&dev->bulk_in_completion);
-		if (rv < 0)
-			goto exit;
-		/*
-		 * by waiting we also semiprocessed the urb
-		 * we must finish now
-		 */
-		dev->bulk_in_copied = 0;
-		dev->processed_urb = 1;
-	}
-
-	if (!dev->processed_urb) {
-		/*
-		 * the URB hasn't been processed
-		 * do it now
-		 */
-		wait_for_completion(&dev->bulk_in_completion);
-		dev->bulk_in_copied = 0;
-		dev->processed_urb = 1;
-	}
-
-	/* errors must be reported */
-	rv = dev->errors;
-	if (rv < 0) {
-		/* any error is reported once */
-		dev->errors = 0;
-		/* to preserve notifications about reset */
-		rv = (rv == -EPIPE) ? rv : -EIO;
-		/* no data to deliver */
-		dev->bulk_in_filled = 0;
-		/* report it */
-		goto exit;
-	}
-
-	/*
-	 * if the buffer is filled we may satisfy the read
-	 * else we need to start IO
-	 */
-
-	if (dev->bulk_in_filled) {
-		/* we had read data */
-		size_t available = dev->bulk_in_filled - dev->bulk_in_copied;
-		size_t chunk = min(available, count);
-
-		if (!available) {
-			/*
-			 * all data has been used
-			 * actual IO needs to be done
-			 */
-			rv = t20aiptek_do_read_io(dev, count);
-			if (rv < 0)
-				goto exit;
-			else
-				goto retry;
-		}
-		/*
-		 * data is available
-		 * chunk tells us how much shall be copied
-		 */
-
-		if (copy_to_user(buffer,
-				 dev->bulk_in_buffer + dev->bulk_in_copied,
-				 chunk))
-			rv = -EFAULT;
-		else
-			rv = chunk;
-
-		dev->bulk_in_copied += chunk;
-
-		/*
-		 * if we are asked for more than we have,
-		 * we start IO but don't wait
-		 */
-		if (available < count)
-			t20aiptek_do_read_io(dev, count - chunk);
-	} else {
-		/* no data in the buffer */
-		rv = t20aiptek_do_read_io(dev, count);
-		if (rv < 0)
-			goto exit;
-		else if (!(file->f_flags & O_NONBLOCK))
-			goto retry;
-		rv = -EAGAIN;
-	}
-exit:
-	mutex_unlock(&dev->io_mutex);
-	return rv;
-}
-
-static void t20aiptek_write_bulk_callback(struct urb *urb)
-{
-	struct usb_t20aiptek *dev;
-
-	dev = urb->context;
-
-	/* sync/async unlink faults aren't errors */
-	if (urb->status) {
-		if (!(urb->status == -ENOENT ||
-		    urb->status == -ECONNRESET ||
-		    urb->status == -ESHUTDOWN))
-			err("%s - nonzero write bulk status received: %d",
-			    __func__, urb->status);
-
-		spin_lock(&dev->err_lock);
-		dev->errors = urb->status;
-		spin_unlock(&dev->err_lock);
-	}
-
-	/* free up our allocated buffer */
-	usb_free_coherent(urb->dev, urb->transfer_buffer_length,
-			  urb->transfer_buffer, urb->transfer_dma);
-	up(&dev->limit_sem);
-}
-
-static ssize_t t20aiptek_write(struct file *file, const char *user_buffer,
-			  size_t count, loff_t *ppos)
-{
-	struct usb_t20aiptek *dev;
-	int retval = 0;
-	struct urb *urb = NULL;
-	char *buf = NULL;
-	size_t writesize = min(count, (size_t)MAX_TRANSFER);
-
-	dev = file->private_data;
-
-	/* verify that we actually have some data to write */
-	if (count == 0)
-		goto exit;
-
-	/*
-	 * limit the number of URBs in flight to stop a user from using up all
-	 * RAM
-	 */
-	if (!(file->f_flags & O_NONBLOCK)) {
-		if (down_interruptible(&dev->limit_sem)) {
-			retval = -ERESTARTSYS;
-			goto exit;
-		}
-	} else {
-		if (down_trylock(&dev->limit_sem)) {
-			retval = -EAGAIN;
-			goto exit;
-		}
-	}
-
-	spin_lock_irq(&dev->err_lock);
-	retval = dev->errors;
-	if (retval < 0) {
-		/* any error is reported once */
-		dev->errors = 0;
-		/* to preserve notifications about reset */
-		retval = (retval == -EPIPE) ? retval : -EIO;
-	}
-	spin_unlock_irq(&dev->err_lock);
-	if (retval < 0)
-		goto error;
-
-	/* create a urb, and a buffer for it, and copy the data to the urb */
-	urb = usb_alloc_urb(0, GFP_KERNEL);
-	if (!urb) {
-		retval = -ENOMEM;
-		goto error;
-	}
-
-	buf = usb_alloc_coherent(dev->udev, writesize, GFP_KERNEL,
-				 &urb->transfer_dma);
-	if (!buf) {
-		retval = -ENOMEM;
-		goto error;
-	}
-
-	if (copy_from_user(buf, user_buffer, writesize)) {
-		retval = -EFAULT;
-		goto error;
-	}
-
-	/* this lock makes sure we don't submit URBs to gone devices */
-	mutex_lock(&dev->io_mutex);
-	if (!dev->interface) {		/* disconnect() was called */
-		mutex_unlock(&dev->io_mutex);
-		retval = -ENODEV;
-		goto error;
-	}
-
-	/* initialize the urb properly */
-	usb_fill_bulk_urb(urb, dev->udev,
-			  usb_sndbulkpipe(dev->udev, dev->bulk_out_endpointAddr),
-			  buf, writesize, t20aiptek_write_bulk_callback, dev);
-	urb->transfer_flags |= URB_NO_TRANSFER_DMA_MAP;
-	usb_anchor_urb(urb, &dev->submitted);
-
-	/* send the data out the bulk port */
-	retval = usb_submit_urb(urb, GFP_KERNEL);
-	mutex_unlock(&dev->io_mutex);
-	if (retval) {
-		err("%s - failed submitting write urb, error %d", __func__,
-		    retval);
-		goto error_unanchor;
-	}
-
-	/*
-	 * release our reference to this urb, the USB core will eventually free
-	 * it entirely
-	 */
-	usb_free_urb(urb);
-
-
-	return writesize;
-
-error_unanchor:
-	usb_unanchor_urb(urb);
-error:
-	if (urb) {
-		usb_free_coherent(dev->udev, writesize, buf, urb->transfer_dma);
-		usb_free_urb(urb);
-	}
-	up(&dev->limit_sem);
-
-exit:
-	return retval;
-}
-
-static const struct file_operations t20aiptek_fops = {
-	.owner =	THIS_MODULE,
-	.read =		t20aiptek_read,
-	.write =	t20aiptek_write,
-	.open =		t20aiptek_open,
-	.release =	t20aiptek_release,
-	.flush =	t20aiptek_flush,
-	.llseek =	noop_llseek,
-};
-
-/*
- * usb class driver info in order to get a minor number from the usb core,
- * and to have the device registered with the driver core
- */
-static struct usb_class_driver t20aiptek_class = {
-	.name =		"t20aiptek%d",
-	.fops =		&t20aiptek_fops,
-	.minor_base =	USB_T20AIPTEK_MINOR_BASE,
-};
-
-static int t20aiptek_probe(struct usb_interface *interface,
+static int t20_probe(struct usb_interface *interface,
 		      const struct usb_device_id *id)
 {
 	struct usb_t20aiptek *dev;
@@ -650,42 +211,95 @@ static int t20aiptek_probe(struct usb_interface *interface,
 
 	
 	/*frambuffer initialisation :*/
-	
-	/* we can register the device now, as it is ready */
-	retval = usb_register_dev(interface, &t20aiptek_class);
-	if (retval) {
-		/* something prevented us from registering this driver */
-		err("Not able to get a minor for this device.");
-		usb_set_intfdata(interface, NULL);
+	retval = t20aiptek_fb_init(dev)
+	if(retval){
+		err("Problems during framebuffer initalisation. Scheiße!");
 		goto error;
 	}
-
-	/* let the user know what node this device is now attached to */
-	dev_info(&interface->dev,
-		 "USB Skeleton device now attached to USBSkel-%d",
-		 interface->minor);
 	return 0;
 
 error:
 	if (dev)
 		/* this frees allocated memory */
-		kref_put(&dev->kref, t20aiptek_delete);
+		kref_put(&dev->kref, t20_delete);
 	return retval;
 }
+
+static void t20_fillrect(struct fb_info *info, const struct fb_fillrect *rect);
+static void t20_copyarea(struct fb_info *info, const struct fb_copyarea *area);
+static void t20_imageblit(struct fb_info *info, const struct fb_image *image);
+static int t20_blank(int blank_mode, struct fb_info *fbi);
+
 
 static struct fb_ops t20aiptekfb_ops =
 {
 	.owner			= THIS_MODULE,
-	.fb_setcolreg		= t20aiptekfb_setcolreg,
-	.fb_blank		= t20aiptekfb_blank,
-	.fb_fillrect		= cfb_fillrect,
-	.fb_copyarea		= cfb_copyarea,
-	.fb_imageblit		= cfb_imageblit,
+	.fb_setcolreg		= t20_setcolreg,
+	.fb_blank		= t20_blank,
+	.fb_fillrect		= t20_fillrect,
+	.fb_copyarea		= t20_copyarea,
+	.fb_imageblit		= t20_imageblit,
 };
+
+
+static int
+t20_blank(int blank_mode, struct fb_info *fbi)
+{
+	struct usb_t20aiptek *drvdata = fbinfo_to_t20aiptek_dev(fbi);
+
+	switch (blank_mode) {
+	case FB_BLANK_UNBLANK:
+		/* turn on panel */
+		//TODO
+		break;
+
+	case FB_BLANK_NORMAL:
+	case FB_BLANK_VSYNC_SUSPEND:
+	case FB_BLANK_HSYNC_SUSPEND:
+	case FB_BLANK_POWERDOWN:
+		/* turn off panel */
+		//TODO	
+	default:
+		break;
+
+	}
+	return 0; /* success */
+}
+
+static void t20_fillrect(struct fb_info *info, const struct fb_fillrect *rect){
+	sys_fillrect(info, rect);
+	t20_sendimage();
+}
+
+static void t20_copyarea(struct fb_info *info, const struct fb_copyarea *area){
+	sys_copyarea(info , area);
+	t20_sendimage();
+}
+
+static void t20_imageblit(struct fb_info *info, const struct fb_image *image){
+	sys_imageblit(info,image);
+	t20_sendimage();
+}
 
 static void t20aiptek_fb_init(struct usb_t20aiptek * drvdata)
 {
-		/* Fill struct fb_info */
+	int rc;
+	size_t fbsize = t20aiptek_fb_var.xvirt * t20aiptek_fb_var.yvirt * BYTES_PER_PIXEL;
+
+	/* allocate frambuffer memory */
+	drvdata->fb_virt = dma_alloc_coherent(dev, PAGE_ALIGN(fbsize),
+			&drvdata->fb_phys, GFP_KERNEL);
+	if (!drvdata->fb_virt) {
+		dev_err(dev, "Could not allocate frame buffer memory\n");
+		rc = -ENOMEM;
+		goto err_nallok;
+	}
+
+	/* Clear (turn to black) the framebuffer */
+	memset_io((void __iomem *)drvdata->fb_virt, 0, fbsize);
+
+
+	/* Fill struct fb_info */
 	drvdata->info.device = dev;
 	drvdata->info.screen_base = (void __iomem *)drvdata->fb_virt;
 	drvdata->info.fbops = &t20aiptekfb_ops;
@@ -693,17 +307,53 @@ static void t20aiptek_fb_init(struct usb_t20aiptek * drvdata)
 	drvdata->info.fix.smem_start = drvdata->fb_phys;
 	drvdata->info.fix.smem_len = fbsize;
 	drvdata->info.fix.line_length = pdata->xvirt * BYTES_PER_PIXEL;
-
 	drvdata->info.pseudo_palette = drvdata->pseudo_palette;
 	drvdata->info.flags = FBINFO_DEFAULT;
 	drvdata->info.var = t20aiptek_fb_var;
-	
 
 
-static void t20aiptek_disconnect(struct usb_interface *interface)
+	/* Allocate a colour map */
+	rc = fb_alloc_cmap(&drvdata->info.cmap, PALETTE_ENTRIES_NO, 0);
+	if (rc) {
+		dev_err(dev, "Fail to allocate colormap (%d entries)\n",
+				PALETTE_ENTRIES_NO);
+		goto err_cmap;
+	}
+
+	/* Register new frame buffer */
+	rc = register_framebuffer(&drvdata->info);
+	if (rc) {
+		dev_err(dev, "Could not register frame buffer\n");
+		goto err_regfb;
+	}
+	/* Put a banner in the log (for DEBUG) */
+	dev_dbg(dev, "fb: phys=%llx, virt=%p, size=%x\n",
+			(unsigned long long)drvdata->fb_phys, drvdata->fb_virt, fbsize);
+
+	return 0;	/* success */
+
+err_regfb:
+	fb_dealloc_cmap(&drvdata->info.cmap);
+
+err_cmap:
+	dma_free_coherent(dev, PAGE_ALIGN(fbsize), drvdata->fb_virt,
+			drvdata->fb_phys);
+
+	/* Turn off the display */
+	//TODO
+
+err_nallok:
+	kfree(drvdata);
+	dev_set_drvdata(dev, NULL);
+
+	return rc;
+}
+
+
+
+static void t20_disconnect(struct usb_interface *interface)
 {
 	struct usb_t20aiptek *dev;
-	int minor = interface->minor;
 
 	dev = usb_get_intfdata(interface);
 	usb_set_intfdata(interface, NULL);
@@ -719,66 +369,34 @@ static void t20aiptek_disconnect(struct usb_interface *interface)
 	usb_kill_anchored_urbs(&dev->submitted);
 
 	/* decrement our usage count */
-	kref_put(&dev->kref, t20aiptek_delete);
+	kref_put(&dev->kref, t20_delete);
 
 	dev_info(&interface->dev, "USB Skeleton #%d now disconnected", minor);
 }
 
-static void t20aiptek_draw_down(struct usb_t20aiptek *dev)
+
+static int t20_fb_release(struct usb_t20aiptek *drvdata)
 {
-	int time;
 
-	time = usb_wait_anchor_empty_timeout(&dev->submitted, 1000);
-	if (!time)
-		usb_kill_anchored_urbs(&dev->submitted);
-	usb_kill_urb(dev->bulk_in_urb);
-}
+	//TODO maybe do blanking stuff
 
-static int t20aiptek_suspend(struct usb_interface *intf, pm_message_t message)
-{
-	struct usb_t20aiptek *dev = usb_get_intfdata(intf);
+	unregister_framebuffer(&drvdata->info);
+	fb_dealloc_cmap(&drvdata->info.cmap);
+	dma_free_coherent(dev, PAGE_ALIGN(drvdata->info.fix.smem_len),
+				  drvdata->fb_virt, drvdata->fb_phys);
 
-	if (!dev)
-		return 0;
-	t20aiptek_draw_down(dev);
-	return 0;
-}
-
-static int t20aiptek_resume(struct usb_interface *intf)
-{
-	return 0;
-}
-
-static int t20aiptek_pre_reset(struct usb_interface *intf)
-{
-	struct usb_t20aiptek *dev = usb_get_intfdata(intf);
-
-	mutex_lock(&dev->io_mutex);
-	t20aiptek_draw_down(dev);
-
-	return 0;
-}
-
-static int t20aiptek_post_reset(struct usb_interface *intf)
-{
-	struct usb_t20aiptek *dev = usb_get_intfdata(intf);
-
-	/* we are sure no URBs are active - no locking needed */
-	dev->errors = -EPIPE;
-	mutex_unlock(&dev->io_mutex);
-
+	kfree(drvdata);
+	dev_set_drvdata(dev, NULL);
 	return 0;
 }
 
 static struct usb_driver t20aiptek_driver = {
 	.name =		"t20aiptek",
-	.probe =	t20aiptek_probe,
-	.disconnect =	t20aiptek_disconnect,
-	.suspend =	t20aiptek_suspend, //Maybe we cant
-	.resume =	t20aiptek_resume,  //do this ?? -> more reversing needed
-	.pre_reset =	t20aiptek_pre_reset,
-	.post_reset =	t20aiptek_post_reset,
-	.id_table =	t20aiptek_table,
+	.probe =	t20_probe,
+	.disconnect =	t20_disconnect,
+	.suspend =	t20_suspend, //Maybe we cant
+	.resume =	t20_resume,  //do this ?? -> more reversing needed
+	.id_table =	t20_table,
 	.supports_autosuspend = 1,
 };
 
