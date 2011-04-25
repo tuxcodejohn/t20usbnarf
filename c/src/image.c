35d1123f09e6674bde4b32d717b0f1d6b003d62c
Thammi <thammi@chaossource.net>
2011-04-25 21:25:04 +0200

    c: Keeping aspect ration in read_image()

--- c/src/image.c
+++ c/src/image.c
@@ -1,5 +1,7 @@
 #include "image.h"
 
+#include "beamer.h"
+
 int create_image(image_handle* handle) {
 	MagickWandGenesis();
 	*handle = NewMagickWand();
@@ -7,7 +9,10 @@ int create_image(image_handle* handle) {
 }
 
 int read_image(image_handle handle, const char* file_name, char* data) {
+	size_t scale_width, scale_height;
+	size_t cur_width, cur_height;
 	MagickBooleanType status;
+	PixelWand* pixel = NewPixelWand();
 
 	puts(file_name);
 
@@ -17,9 +22,29 @@ int read_image(image_handle handle, const char* file_name, char* data) {
 		return -1;
 	}
 
+	cur_width = MagickGetImageWidth(handle);
+	cur_height = MagickGetImageHeight(handle);
+
+	if(cur_width * FRAME_HEIGHT / cur_height <= FRAME_WIDTH) {
+		scale_width = cur_width * FRAME_HEIGHT / cur_height;
+		scale_height = FRAME_HEIGHT;
+	} else {
+		scale_width = FRAME_WIDTH;
+		scale_height = cur_height * FRAME_WIDTH / cur_width;
+	}
+
 	MagickResetIterator(handle);
 	while(MagickNextImage(handle) != MagickFalse) {
-		MagickResizeImage(handle, 640, 480, LanczosFilter, 1.0);
+		MagickResizeImage(handle, scale_width, scale_height, LanczosFilter, 1.0);
+	}
+
+	PixelSetColor(pixel, "black");
+	MagickSetImageBackgroundColor(handle, pixel);
+
+	status = MagickExtentImage(handle, FRAME_WIDTH, FRAME_HEIGHT, (FRAME_WIDTH - scale_width) / 2, (FRAME_HEIGHT - scale_height) / 2);
+
+	if(status == MagickFalse) {
+		return -1;
 	}
 
 	status = MagickExportImagePixels(handle, 0, 0, 640, 480, "BGR", CharPixel, data);
